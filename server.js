9edb5e873b760d12ed8cc07739c55788e355469c
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-19 21:57:37 -0400

    Modified Databse link

--- server.js
+++ server.js
@@ -18,12 +18,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/ProgressiveBudget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/ProgressiveBudget',
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
